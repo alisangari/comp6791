43f7680262bddd60097a9315940f08a517f57b50
Ali <alisangari@butbucket.org>
2015-10-13 23:11:18 -0400

    indexer

--- Project1WebSearch/src/index/Indexer.java
+++ Project1WebSearch/src/index/Indexer.java
@@ -9,8 +9,11 @@ import article.Article;
 public class Indexer {
 
 	public static HashMap<String, ArrayList<Integer>> index(Article article) {
+
+		article.content = normalize(article.content, true, true, true);
 		String[] terms = article.content.split(" ");
-		terms = normalize(terms);
+		terms = Cleanse(terms);
+//		terms = normalize(terms, true);
 		HashSet<String> tokens = tokenize(terms);
 		HashMap<String, ArrayList<Integer>> res = new HashMap<String, ArrayList<Integer>>();
 
@@ -29,9 +32,41 @@ public class Indexer {
 		return res;
 	}
 
-	private static String[] normalize(String[] terms) {
+	private static String[] Cleanse(String[] terms) {
+		String[] temp = new String[terms.length];
+		int c = 0;
 		for (String str : terms) {
+			if (!str.trim().equals("")) {
+				temp[c] = str;
+				c++;
+			}
+		}
+		String[] res = new String[c];
+		for (int i = 0; i < c; i++) {
+			res[i] = temp[i];
+		}
+		return res;
+	}
+
+	private static String normalize(String str, boolean toLower,
+			boolean removeDigits, boolean removeNonAlphaNumeric) {
+		if (removeNonAlphaNumeric) {
+			str = str.replaceAll("[,._\\-()\"'&#;]", "");
+		}
+		if (removeDigits) {
+			str = str.replaceAll("[0-9]", "");
+		}
+		if (toLower) {
 			str = str.toLowerCase();
+		}
+		return str;
+	}
+
+	private static String[] normalize(String[] terms, boolean toLower) {
+		if (toLower) {
+			for (int i = 0; i < terms.length; i++) {
+				terms[i] = terms[i].toLowerCase();
+			}
 		}
 		return terms;
 	}
